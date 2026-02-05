61b9159cb37fd75c3c4bd20b6f887f85fc70f41d
Broderick Johnson <brodjohn22@gmail.com>
2026-02-04 23:33:01 -0300

    more file structure changes and jsx additions

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+
+export default function App(){
+    return (
+        <div>
+            <div>placeholder</div>
+            <footer class="flex justify-center mb-3">
+                <p class="italic content-center">Author: Broderick Johnson</p>
+                <a class="ml-20 underline content-center text-stone-300 hover:text-cyan-700" href="https://github.com/antnest8/startup">GitHub</a>
+                <img class="ml-2" src="github.svg" type="image/svg+xml"/>
+            </footer>
+        </div>  
+    );
+}
