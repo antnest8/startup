3abbd061061db6593317703c57dacfd24311e518
Broderick Johnson <brodjohn22@gmail.com>
2026-02-04 23:36:53 -0300

    imported component stubs to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Office } from './office/office';
+import { Settings } from './settings/settings';
+
 
 export default function App(){
     return (
@@ -8,7 +13,7 @@ export default function App(){
             <footer class="flex justify-center mb-3">
                 <p class="italic content-center">Author: Broderick Johnson</p>
                 <a class="ml-20 underline content-center text-stone-300 hover:text-cyan-700" href="https://github.com/antnest8/startup">GitHub</a>
-                <img class="ml-2" src="github.svg" type="image/svg+xml"/>
+                <img class="ml-2" src="../public/github.svg" type="image/svg+xml"/>
             </footer>
         </div>  
     );
