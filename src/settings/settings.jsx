e6f538a4c80a6e15c3193451b7f9fa57d4359135
Broderick Johnson <brodjohn22@gmail.com>
2026-03-24 00:36:21 -0300

    finished with adding account deletion

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DataField} from './dataField';
 import { NavBarButton } from '../nav/barButtons.jsx';
+import { useNavigate} from 'react-router-dom';
 
 export function Settings(props){
     // TODO: pass data into Settings and handle
@@ -44,22 +45,36 @@ export function Settings(props){
                         <DataField purpose="displayName" fieldData={userDisplay} changeInitialsFunction={changeUserInitials} changeFunction={changeUserDisplay} fieldDisplayName="Display Name"/>
                         <DataField purpose="email" fieldData={userEmail} changeFunction={changeUserEmail} fieldDisplayName="Account Email"/>
                         <DataField purpose="password" fieldData={userPass} changeFunction={()=>false} fieldDisplayName="Password"/>
-                        <div className="flex">
+                        <div className="flex justify-center">
                             <button className="rounded-lg mt-2 outline-solid outline-stone-700 content-center text-center hover:outline-teal-700 hover:bg-teal-950 w-30 h-10 bg-stone-900 mx-3 cursor-pointer" onClick={()=>{setDeletingAccount(true)}}>Delete Account</button>
                         </div>
                     </div>
                 </div>
-                {deletingAccount && <AccountDeletionConfirmation/>}
+                {deletingAccount && <AccountDeletionConfirmation undoFunction={setDeletingAccount}/>}
             </main>
         </div>
     )
 }
 
-function AccountDeletionConfirmation(){
+function AccountDeletionConfirmation(props){
+    const setDeletingAccount = props.undoFunction;
+    const nav = useNavigate();
+
+    async function sendDeleteReq(){
+        await fetch('api/user/data',{
+            method: "DELETE",
+        })
+
+        nav('/');
+    }
 
     return(
-        <aside className="flex flex-col p-8 px-12 bg-rose-900 rounded-md grow max-w-[70dvw] max-h-[90dvw] mb-6 mt-2">
-
+        <aside className="flex flex-col justify-evenly border-rose-950 border-5 border-solid p-8 px-12 bg-rose-900 rounded-md fixed top-1/2 left-1/2 min-w-[60dvw] min-h-[20dvw] translate-[-50%]">
+            <p className='text-center text-stone-300 text-xl font-bold'>Are you sure you want to delete your account?</p>
+            <div className='flex justify-evenly'>
+                <button className="rounded-lg mt-2 outline-solid outline-[#0000009A] content-center text-center hover:outline-amber-600 hover:bg-amber-900 w-30 h-10 bg-rose-950 mx-3 cursor-pointer" onClick={()=>{setDeletingAccount(false)}}>No</button>
+                <button className="rounded-lg mt-2 outline-solid outline-[#0000009A] content-center text-center hover:outline-amber-600 hover:bg-amber-900 w-30 h-10 bg-rose-950 mx-3 cursor-pointer" onClick={sendDeleteReq}>Yes</button>
+            </div>
         </aside>
     );
 }
