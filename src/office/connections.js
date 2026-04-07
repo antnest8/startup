71dfec72be5ad38865465252ff897df6e4530cad
Broderick Johnson <brodjohn22@gmail.com>
2026-04-07 00:15:25 -0300

    gutted the Connections code and added boilerplate code

--- src/office/connections.js
+++ src/office/connections.js
@@ -1,51 +1,13 @@
+
+
 class Connections{
-    onlineUsers = [];
-    handler;
+    onlineUsers = {};
+    handlers = [];
+    socket;
 
     constructor(){
-        const testUser = {
-            userName: "otherDude",
-            initials: "OD",
-            displayName: "Other Dude!",
-            x : 50,
-            y : 50,
-            isTalking: true, //fix later
-        }
-
-        fetch("https://api.dicebear.com/9.x/avataaars-neutral/svg?seed=OD&radius=50")
-            .then((res)=>res.text())
-            .then((res)=>testUser.userImage = { __html: res})
-            .finally(
-                setInterval(()=>{
-                    //updateMovement
-                    if(this.handler){
-                        testUser.x = Math.random() * 90 + 5;
-                        testUser.y = Math.random() * 90 + 5;
-                        this.handler([testUser])
-                    }
-                },10000)
-            );
-
-        
-    }
-
-    connectSelf(userData, handler){
-        console.log("OFFICE-CONNECTIONS: User Connected");
-        this.handler = handler;
-        const testUser = {
-            userName: "otherDude",
-            initials: "OD",
-            displayName: "Other Dude!",
-            x : 25,
-            y : 25,
-            isTalking: true, //fix later
-        }
-        //handler([testUser])
-        return 
-    }
-
-    pushData(userData){
-        console.log("OFFICE-CONNECTIONS: Data Recieved");
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
     }
     
 }
